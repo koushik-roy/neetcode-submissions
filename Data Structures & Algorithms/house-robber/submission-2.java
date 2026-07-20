f6f8f07c263e7b1cc9491355860fe7be86fb2214
Koushik Roy <jc.koushikroy@gmail.com>
2026-07-20 08:50:52 +0530

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n + 2];
+
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = Math.max(nums[i] + dp[i + 2], dp[i + 1]);
+        }
+
+        return Math.max(dp[0], dp[1]);
+    }
+}
