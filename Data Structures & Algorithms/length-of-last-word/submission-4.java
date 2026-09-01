974f80778ebc43a7368b1f7aef085bb50c1e6b10
Koushik Roy <jc.koushikroy@gmail.com>
2026-09-01 16:32:20 +0530

    Add: length-of-last-word - submission-4

---
+++ Data Structures & Algorithms/length-of-last-word/submission-4.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int n = s.length() - 1;
+        int i = n;
+        int count = 0;
+        if(n==0){
+            return 1;
+        }
+        if (s.charAt(i) == ' ') {
+            while (s.charAt(i) == ' ') {
+                i--;
+            }
+        }
+
+        while (s.charAt(i) != ' ' && i>0) {
+            count++;
+            i--;
+        }
+
+        System.out.println(n);
+        System.out.println(i);
+
+        return count;
+    }
+}
